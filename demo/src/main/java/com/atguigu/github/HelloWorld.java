7919f6b6444c4fc996d6abba0543cb1f23df8314
luv <11192163@qq.com>
2021-10-31 19:36:02 +0800

    练习提交操作

---
+++ demo/src/main/java/com/atguigu/github/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.atguigu.github;
+
+/**
+ * @author lllllc Email:fddm.wxy@gmail.com
+ * @create 2021- 10- 31  19:24
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
